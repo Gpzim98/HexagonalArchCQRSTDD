fd506a162175f2d639f231512acfb1c505f16640
Your Name <you@example.com>
2023-02-05 09:36:46 +0000

    command handler

---
+++ PublicWebSite/CommandHandlers.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace PublicWebSite
+{
+    public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, CustomerResponse>
+    {
+        public Task<CustomerResponse> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
