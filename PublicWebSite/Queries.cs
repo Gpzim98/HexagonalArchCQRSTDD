7b1e0c3a4bcd7a00723c5e459a0a57dae07ee941
Your Name <you@example.com>
2023-02-05 10:13:14 +0000

    getcustomer query

---
+++ PublicWebSite/Queries.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace PublicWebSite
+{
+    public class GetCustomerQuery : IRequest<CustomerResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ PublicWebSite/QueryHandlers.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace PublicWebSite
+{
+    public class GetCustomerHandler : IRequestHandler<GetCustomerQuery, CustomerResponse>
+    {
+        public Task<CustomerResponse> Handle(GetCustomerQuery query, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
